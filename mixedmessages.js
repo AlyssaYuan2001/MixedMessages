8177e75eecbd0559847ca62fc7d2069b32bf1a8b
AlyssaYuan2001 <78182312+AlyssaYuan2001@users.noreply.github.com>
2022-02-10 22:33:26 -0500

    make object and method

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,1 +1,10 @@
-console.log('mixed messages')
+const fortuneTeller = {
+    yourProblems = ['You are getting bad grades', 'You are inconsistent with your exercising', 'You suck at leetcode', 'You are bad at talk to people', 'You dont have a friend/support group', 'You are a sow learner'],
+    reason = ['You dont work smart consistently', 'You keep prioritizing pointless things', 'You do not proportize the right things', 'You think you have all time in the world', 'You are too inside your head; people dont think about you', 'It takes time to learn something'],
+    advice = ['Slow and steady wins the race', 'Make sure you eating, exercsing, and sleeping enough', 'try talk to new people irl(walk up to people)', 'getting a tracker', 'really find your "why"', 'think about your goal'],
+    messageGen() {
+        for(let i=0; i < 7; i++) {
+            
+        }
+    }
+}
