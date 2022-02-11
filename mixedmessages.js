fdaf74a9909fc0dd43bac882350032a01d84972b
AlyssaYuan2001 <78182312+AlyssaYuan2001@users.noreply.github.com>
2022-02-11 08:28:59 -0500

    for..in loop to get a random index for each property's key to get a random value using fortuneTeller[item][randomIndex]

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,21 +1,31 @@
 const fortuneTeller = {
-    _problems: ['You are getting bad grades.', 'You are inconsistent with your exercising.', 'You suck at leetcode.', 'You are bad at talk to people.', 'You dont have a friend/support group.', 'You are a sow learner.'],
-    _reasons: ['You dont work smart consistently.', 'You keep prioritizing pointless things.', 'You do not proportize the right things.', 'You think you have all time in the world.', 'You are too inside your head; people dont think about you.', 'It takes time to learn something.'],
-    _advices: ['Slow and steady wins the race.', 'Make sure you eating, exercsing, and sleeping enough.', 'try talk to new people irl(walk up to people.', 'getting a tracker.', 'really find your "why".', 'think about your goal.'],
-    get problems() {
-        return this._problems
-    },
-    get reasons() {
-        return this._reasons
-    },
-    get advices() {
-        return this._advices
+    problems: ['You are getting bad grades.', 'You are inconsistent with your exercising.', 'You suck at leetcode.', 'You are bad at talk to people.', 'You dont have a friend/support group.', 'You are a sow learner.'],
+    reasons: ['You dont work smart consistently.', 'You keep prioritizing pointless things.', 'You do not proportize the right things.', 'You think you have all time in the world.', 'You are too inside your head; people dont think about you.', 'It takes time to learn something.'],
+    advices: ['Slow and steady wins the race.', 'Make sure you eating, exercsing, and sleeping enough.', 'try talk to new people irl(walk up to people.', 'getting a tracker.', 'really find your "why".', 'think about your goal.'],
+}
+let final = []
+const randomNum = (num) => {
+    return Math.floor(Math.random() * num)
+}
+const createMessage = () =>{
+    for (const item in fortuneTeller) {
+        let randomIndex = randomNum(fortuneTeller[item].length)
+        switch (item) {
+            case 'problems':
+                final.push(`What is your problem: ${fortuneTeller[item][randomIndex]}`)
+                break;
+            case 'reasons':
+                final.push(`The reason you might be experiencing that: ${fortuneTeller[item][randomIndex]}`)
+                break;
+            case 'advices':
+                final.push(`Recommended action to combat this: ${fortuneTeller[item][randomIndex]}`)
+                break;
+            default:
+                final.push(`error`)
+        }
     }
+    return final
 }
-function randomIndex() {
-    let problem = fortuneTeller._problems[Math.floor(Math.random() * fortuneTeller._problems.length)]
-    let reason = fortuneTeller._reasons[Math.floor(Math.random() * fortuneTeller._reasons.length)]
-    let advice = fortuneTeller._advices[Math.floor(Math.random() * fortuneTeller._advices.length)]
-    return advice
-}
-console.log(randomIndex())
+console.log(createMessage().join(" "))
+//let finalString = createMessage().join(" ") + "."
+//console.log(finalString)
